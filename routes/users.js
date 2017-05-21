41d8400c998a770e2c66a338981185a98374cafd
TiahmSotoudeh <sotoudehtiahm@gmail.com>
2017-05-21 03:18:41 -0700

    probably will break goodluckxdkillmeallmhyfriehndsaredeahd

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require(mongoose);
+var passportLocalMongoose = require("passport-local-mongoose");
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password; String
+});
 
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User",UserSchema);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
