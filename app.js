442f6ac8be426208272cf502f50d8cfab32fb634
evan <evanthurston@gmail.com>
2017-05-21 01:55:49 -0700

    still sucks lol

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var cool = "hi";
+
+// function returnCool() {
+//     return "cool";
+// }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
